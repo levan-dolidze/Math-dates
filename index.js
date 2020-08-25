d37e83edbc9e385b9e65edc576105f490439bf96
DESKTOP-H8VL884\levan <levan.dolidze@outlook.com>
2020-08-25 23:23:32 +0400

    math

---
+++ index.js
@@ -0,0 +1,167 @@
+//vabrunebt random riscvs 0 da 1  shoris
+// const randomNum=Math.random();
+// const testName=25.2365;
+// const roundedTesnNum=Math.round(testName);
+// const roundedDown=Math.floor(testName)
+// const roundedUp=Math.ceil(testName)
+
+
+
+// console.log(Math.pow(testName,2));
+// console.log(Math.sqrt(64))
+// console.log(roundedDown)
+// console.log(roundedTesnNum);
+
+
+
+// prompt('shemoitane cifri')
+// const shemotanili=document.prompt
+
+// const random=Math.floor(Math.random()*100)+1;
+// console.log('your random number is', random)
+
+// for (let index = 0; index < random.length; index++) {
+//   if(shemotanili==random){
+//       console.log('moigee')
+//   }
+//   else{
+//       console.log('waage')
+//   }
+    
+// }
+
+
+//datee
+
+//new heyword is gamogenebit vqmnit date obiekts sadac motavsebulia date function adn properties
+
+// const date=new Date();
+// const date2=new Date(2014,1,15,20,30,25);
+// const date3=new Date(48412235654);
+
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+
+// console.log(date.getHours());
+// console.log(date.getUTCHours());
+// console.log(date.getTime());
+
+
+// const dateerti=new Date(1999)
+// const dateori=new Date(1999)
+
+// const sxvaoba=dateerti-dateori
+
+// console.log(sxvaoba*365);
+
+// console.log(date3)
+// console.log(date);
+// console.log(date2);
+
+//async/////////////////////////
+
+// const timeout= setTimeout(logWelcome,2000);
+// clearTimeout(timeout)
+// console.log(2)
+
+// function logWelcome(){
+//     document.write('welcome user');
+// }
+
+// const interval=setInterval(logWelcome, 2000);
+
+// clearInterval(interval)
+
+// const timeout= setTimeout(logWelcome,2000);
+// // clearTimeout(timeout)
+// // console.log(4)
+
+
+// function logWelcome(){
+//     document.write('welcome user');
+// }
+// const timeout= setTimeout(logWelcome,2000);
+// const interval=setInterval(logWelcome,2000);
+// function logWelcome(){
+//     document.write('welcome user');
+// }
+
+// logWelcome(interval,timeout)
+// clearTimeout(timeout,6000);
+
+
+const array=[
+    {
+        name:'levan',
+        age:12,
+        kle:'rrree'
+    },
+    {
+        name:'levan',
+        age:30,
+        kle:'rrree'
+    },
+    {
+        name:'levan',
+        age:25,
+        kle:'rrree'
+    },
+    {
+        name:'levan',
+        age:'dodo',
+        kle:'rrree'
+    }
+]
+
+// const new11=array.map((Element)=>{
+// return Element.kle
+
+// })
+
+
+// array.forEach(element => {
+//     console.log(element)
+    
+// });
+
+// const tes = array.some((element)=>{
+// return element.age>=25;
+// })
+
+
+// console.log(tes)
+// // console.log(new11)
+
+
+// function log(name){
+// console.log(name)
+// }
+// log('lev')
+
+// const log =(age)=>{
+// console.log(age)
+// }
+// log(24);
+
+
+// const test=(age=10,name='levan')=>{
+//     const welcome=`rogor xar ${name} shen ${age} wlis xar?`
+    
+//     console.log(welcome)
+// }
+// test()
+
+
+const obj ={
+    name:'sofo',
+    age:14,
+    fav:[1,2,3,4,5,6]
+}
+
+
+for (let index = 0; index < obj.fav.length; index++) {
+ 
+
+console.log(obj.fav[index])
+
+}
